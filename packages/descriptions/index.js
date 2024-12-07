6188f06dd95adfde5ec327c2658447efcd54f49a
wuyao <2466822404@qq.com>
2024-12-07 21:57:48 +0800

    241207-调整cr格式化问题

---
+++ packages/descriptions/index.js
@@ -0,0 +1,8 @@
+import WDescriptions from "./src/descriptions.vue";
+
+/* istanbul ignore next */
+WDescriptions.install = function (Vue) {
+  Vue.component(WDescriptions.name, WDescriptions);
+};
+
+export default WDescriptions;
---
+++ packages/descriptions/src/descriptions.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "WDescriptions",
+};
+</script>
+
+<template>
+  <div class="w-descriptions">w</div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  box-sizing: border-box;
+}
+</style>
